53a81e67d4f6de978b46dcead49c34fcd493b5ed
roose <roose@mail.ru>
2016-05-10 02:40:19 +0600

    nim binding v0.2.0 and example

---
+++ README.NIM.md
@@ -0,0 +1,35 @@
+# nim-wox
+
+Wox plugins binding and helper Nim package
+
+## Example usage
+
+```nimrod
+import json, wox, strutils, browsers, httpclient
+
+proc getPackages(query: string): JsonNode =
+  let content = getContent("http://npmsearch.com/query?fields=name,description,author,version,homepage&sort=rating:desc&q=name:" & query)
+  return parseJson(content)
+  
+proc query*(query: string) = 
+  # init wox results
+  var result = wox.newResult()
+  let packages = getPackages(query)["results"]
+
+  # parse items and add to results
+  for item in packages:
+    let title = "$1 v$2 by $3" % [item["name"][0].str, item["version"][0].str, item["author"][0].str]
+    let desc = item["description"][0].str
+    let url = item["homepage"][0].str
+    result.addItem(title, desc, "Images\\exe.png", "openUrl", url, false)
+  # print results json
+  echo result.results()
+  
+proc openUrl(url: string) =
+  openDefaultBrowser(url)
+
+when isMainModule:
+  wox.register("query", query)
+  wox.register("openUrl", openUrl)
+  wox.run()
+```
--- wox.nim
+++ wox.nim
@@ -1,45 +1,65 @@
-import json, strutils, sequtils, marshal
+import json, marshal, tables, os
 
-"""
-Helper library for Wox plugin authors
-"""
+## Helper library for Wox plugin authors
+## Version 0.2.0
+## roose 2016
 
 type
   Action = object
-    """Wox JsonRPCAction object"""
+    ## Wox JsonRPCAction object
     `method`: string
-    parameters: string
+    parameters: seq[string]
     dontHideAfterAction: bool
 
   WoxItem = object
-    """Wox item object"""
+    ## Wox item object
     Title: string
     SubTitle: string
     IcoPath: string
     JsonRPCAction: Action
 
-  WoxResult = object
-    """Wox results object"""
+  WoxResult* = object
+    ## Wox results object
     `result`: seq[WoxItem]
 
+  RpcProc* = proc (params: string)
+
+# name, proc table for call proc
+var procs: Table[string, RpcProc] = initTable[string, RpcProc]()
+
+proc register*(name: string, prc: RpcProc) =
+  ## Register proc as name
+  ## :param string name: name to register proc
+  ## :param proc prc: proc
+  procs[name] = prc
+
+proc call*(name, params: string) =
+  ## Call proc by it's name
+  ## :param string name: proc name
+  ## :param string params: proc parameters
+  procs[name](params)
+
+proc run*() =
+  ## Parse JsonRPC from Wox and call method with params
+  let rpcRequest = parseJson(paramStr(1))
+  let requestMethod = rpcRequest["method"].str
+  let requestParams = rpcRequest["parameters"][0].str
+  call(requestMethod, requestParams)
+
 proc newResult*(): WoxResult =
-  """
-  Init WoxResult
-  """
+  ## Init WoxResult
 
   return WoxResult(result: @[])
 
 proc addItem*(self: var WoxResult, title, sub, icon, `method`, params: string, hide: bool = true) =
-  """
-  Add item to the return list
-
-  :param str title: item title
-  :param str sub: item subtitle
-  :param str icon: path to item icon
-  :param str method: a function called when an item is selected
-  :param list params: parameters for callable function
-  :param str hide: hide Wox after select item or not
-  """
+  ## Add item to the return list
+  ##
+  ## :param str title: item title
+  ## :param str sub: item subtitle
+  ## :param str icon: path to item icon
+  ## :param str method: a function called when an item is selected
+  ## :param list params: parameters for callable function
+  ## :param str hide: hide Wox after select item or not
 
   self.result.add(WoxItem(
                     Title: title,
@@ -47,41 +67,11 @@ proc addItem*(self: var WoxResult, title, sub, icon, `method`, params: string, h
                     IcoPath: icon,
                     JsonRPCAction: Action(
                       `method`: `method`,
-                      parameters: params,
+                      parameters: @[params],
                       dontHideAfterAction: hide
                     )
                  ))
 
 proc results*(self: var WoxResult): string =
-  """
-  Return results with all items
-  """
-
+  ## Return results with all items
   return $$self
-
-proc filter*(self: var WoxResult, items: any, key: proc): seq[tuple[text: string, score: float]] =
-  """
-  Search filter
-
-  :param iterable items: any iterable items to search
-  :param proc key: function to create a string for the search query
-  :return seq: sequence of filtred items
-  """
-
-  result = @[]
-  let query = "color"
-
-  for item in items:
-    var score = 0.0
-    var value = key(item)
-    if value == "": continue
-    if value.toLower.startsWith(query):
-      score = 100.0 - (value.len / query.len)
-    elif query in value.toLower:
-      score = 80.0 - (value.len / query.len)
-    if score > 0.0:
-      result.add(($item, score))
-
-  result.sort(proc(x, y: tuple) :int =
-    cmp(x.score, y.score), SortOrder.Descending)
-  return result
